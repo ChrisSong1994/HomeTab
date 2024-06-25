62687a25401434b1bf563a64dcae77c1e44e3b05
ChrisSong <1352411860@qq.com>
2024-06-25 21:23:34 +0800

    fix: 修复搜索推荐连接跳转地址

--- src/utils/search.js
+++ src/utils/search.js
@@ -11,18 +11,24 @@ export const googleSearchSuggestParse = (res, searchKeyword) => {
       return {
         keyword: keyword,
         icon: item?.[3]?.zs,
-        link: `https://www.google.com/search?q=${keyword}`,
+        link: `https://www.google.com/search?q=${encodeURIComponent(keyword)}`,
       };
     });
     // 补充搜索关键词
-    return [
-      {
-        keyword: searchKeyword,
-        icon: null,
-        link: `https://www.google.com/search?q=${searchKeyword}`,
-      },
-      ...data,
-    ];
+    if (data[0].keyword !== searchKeyword) {
+      return [
+        {
+          keyword: searchKeyword,
+          icon: null,
+          link: `https://www.google.com/search?q=${encodeURIComponent(
+            searchKeyword
+          )}`,
+        },
+        ...data,
+      ];
+    } else {
+      return data;
+    }
   }
   return result;
 };
@@ -34,24 +40,29 @@ export const baiduSearchSuggestParse = (res, searchKeyword) => {
     return {
       keyword: keyword,
       icon: null,
-      link: `https://www.baidu.com/s?wd=${keyword}`,
+      link: `https://www.baidu.com/s?wd=${encodeURIComponent(keyword)}`,
     };
   });
   // 补充搜索关键词
-  return [
-    {
-      keyword: searchKeyword,
-      icon: null,
-      link: `https://www.baidu.com/s?wd=${searchKeyword}`,
-    },
-    ...data,
-  ];
+  if (data[0].keyword !== searchKeyword) {
+    return [
+      {
+        keyword: searchKeyword,
+        icon: null,
+        link: `https://www.baidu.com/s?wd=${encodeURIComponent(searchKeyword)}`,
+      },
+      ...data,
+    ];
+  } else {
+    return data;
+  }
 };
 
 export const bingSearchSuggestParse = (res, searchKeyword) => {
   const result = res?.s || [];
   const data = result.map((item) => {
     const keyword = item.q.replaceAll("", "").replaceAll("", "");
+    console.log();
     return {
       keyword: keyword,
       icon: null,
@@ -59,14 +70,18 @@ export const bingSearchSuggestParse = (res, searchKeyword) => {
     };
   });
   // 补充搜索关键词
-  return [
-    {
-      keyword: searchKeyword,
-      icon: null,
-      link: `https://cn.bing.com?q=${encodeURIComponent(searchKeyword)}`,
-    },
-    ...data,
-  ];
+  if (data[0].keyword !== searchKeyword) {
+    return [
+      {
+        keyword: searchKeyword,
+        icon: null,
+        link: `https://cn.bing.com?q=${encodeURIComponent(searchKeyword)}`,
+      },
+      ...data,
+    ];
+  } else {
+    return data;
+  }
 };
 
 export const searchSuggestParse = (type, result, keyword) => {
